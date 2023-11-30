d4159e852c65f3a83c3b84bd15f8da484cca887a
Talicol-Matt <126734056+Talicol-Matt@users.noreply.github.com>
2023-11-30 09:26:06 +0800

    Update ConfigManager.java

--- greenLantern/src/main/java/com/example/greenlantern/ConfigManager.java
+++ greenLantern/src/main/java/com/example/greenlantern/ConfigManager.java
@@ -1,6 +1,24 @@
 package com.example.greenlantern;
 
 public class ConfigManager {
+    private String playerName;
+    private String difficulty;
+
     // Methods to manage game configuration settings
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
 }
 
