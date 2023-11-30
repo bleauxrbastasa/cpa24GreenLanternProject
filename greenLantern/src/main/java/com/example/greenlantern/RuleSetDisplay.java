99877ac624fd15674e0eb78c93f09890f8230918
Talicol-Matt <126734056+Talicol-Matt@users.noreply.github.com>
2023-11-30 09:37:59 +0800

    Update RuleSetDisplay.java

--- greenLantern/src/main/java/com/example/greenlantern/RuleSetDisplay.java
+++ greenLantern/src/main/java/com/example/greenlantern/RuleSetDisplay.java
@@ -4,4 +4,36 @@ import javax.swing.*;
 
 public class RuleSetDisplay extends JPanel {
     // Code to display the current set of rules
+
+    private JTextArea rulesTextArea;
+
+    public RuleSetDisplay() {
+        initializeComponents();
+        layoutComponents();
+        // Load rules into the text area
+        loadRules();
+    }
+
+    private void initializeComponents() {
+        rulesTextArea = new JTextArea();
+        rulesTextArea.setEditable(false);
+        rulesTextArea.setLineWrap(true);
+        rulesTextArea.setWrapStyleWord(true);
+        JScrollPane scrollPane = new JScrollPane(rulesTextArea);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+    }
+
+    private void layoutComponents() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        add(new JLabel("Rules:"));
+        add(rulesTextArea);
+    }
+
+    private void loadRules() {
+        // Place your rules here
+        String rules = "1. Verify the document's dates are valid\n"
+                + "2. Do not accept discreptancies in documents\n"
+                + "3. Work through as many documents as possible\n";
+        rulesTextArea.setText(rules);
+    }
 }
